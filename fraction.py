1efd459be56bc57ad5778d92bd686c665403349e
Võ Ngọc Hoàng Lân <159760133+VoNgocHoangLanUIT@users.noreply.github.com>
2025-03-27 17:43:41 +0700

    Add simplify function to reduce fractions

--- fraction.py
+++ fraction.py
@@ -1,6 +1,13 @@
+from math import gcd
+
 def add_fractions(a_num, a_den, b_num, b_den):
     result_num = a_num * b_den + b_num * a_den
     result_den = a_den * b_den
-    return result_num, result_den
+    return simplify(result_num, result_den)
+
+def simplify(num, den):
+    greatest = gcd(num, den)
+    return num // greatest, den // greatest
 
 print(add_fractions(1, 2, 1, 3))  # (5, 6)
+print(add_fractions(2, 4, 2, 4))  # (1, 1)
